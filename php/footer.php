abc023a18c1513efab009a894568e61db1d017ab
Dmitry I. <dima_@live.com>
2013-12-13 12:01:50 -0500

    update

---
+++ php/footer.php
@@ -0,0 +1,52 @@
+<html>
+	<footer>
+		<div class="wrapper_footer">
+			<div class="logo_contact_links">
+				<div class="logo_footer">
+					<img src="images/logo_small.png">
+				</div>
+
+				<div class="contact">
+					<ul>
+						<li>WWCC Main Office</li>
+						<li>533A Gladstone Avenue</li>
+						<li>Toronto, Ontario M6H 3J1</li>
+						<li>Phone # 416-532-2824/Fax # 416-532-1065</li>
+						<li>Email: admin@workingwomencc.org</li>
+					</ul>
+				</div>
+
+				<div class="extra_links">
+					<ul>
+						<li><a href="#">Home</a></li>
+						<li><a href="#">Contact Us</a></li>
+						<li><a href="#">Careers</a></li>
+						<li><a href="#">Helpful Resources</a></li>
+						<li><a href="#">Privacy/Terms</a></li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="donate_social">
+				<div class="donate_footer">
+					<h2>Donate <span class="pinkText">Now</span></h2>
+				</div>
+
+				<div class="social_footer">
+					<ul>
+						<li><a target="blank" href="https://www.facebook.com/WorkingWomenCommunityCentre"><img src="images/facebooksmall.jpg"></a></li>
+						<li><a href="#"><img src="images/twittersmall.jpg"></a></li>
+						<li><a target="blank" href="http://www.youtube.com/channel/UCnArFwwXd3I1sJJAuBAQwvQ"><img src="images/youtubesmall.jpg"></a></li>
+						<li><a href="#"><img src="images/linkedinsmall.jpg"></a></li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="unitedway">
+				<img src="images/unitedwaylogo.png">
+			</div>
+
+		</div>
+
+	</footer>
+</html>
---
+++ php/header.php
@@ -0,0 +1,19 @@
+
+<html>
+<nav>
+		<ul class="main_nav">
+			<li>Home</li>
+			<li>What We Do</li>
+			<li>Programs & Services</li>
+			<li>Why Give</li>
+			<li>About Us</li>
+		</ul>
+	</nav>
+
+	<header>
+
+		<div class="logo">
+			<img src="images/logo.png">
+		</div>
+	</header>
+</html>
